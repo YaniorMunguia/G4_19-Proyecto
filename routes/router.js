3480aeefaec96eb75fa1a7e2042225032d676be5
AlejandroSanchez017 <139077342+AlejandroSanchez017@users.noreply.github.com>
2023-07-18 20:31:45 -0600

    correccion en url de router

--- routes/router.js
+++ routes/router.js
@@ -13,28 +13,38 @@ router
   .post("/libros/insertar/:codigo_libro", LibrosController.post)
   .put("/libros/actualizar/:codigo_libro", LibrosController.put)
   .delete("/libros/eliminar/:codigo_libro", LibrosController.delete)
- 
+  .use(LibrosController.error404);
 
+//RUTA ESCRITOR
+var EscritorController = require("../controllers/escritor-controller"),
+  express = require("express"),
+  router = express.Router();
+
+router
   //*** ESCRITOR ***
   .get("/escritor/getall", EscritorController.getAll)
   .get("/escritor/getone/:numero_escritor", EscritorController.getOne)
   .post("/escritor/insertar/:numero_escritor", EscritorController.post)
   .put("/escritor/actualizar/:numero_escritor", EscritorController.put)
   .delete("/escritor/eliminar/:numero_escritor", EscritorController.delete)
-  
+  .use(EscritorController.error404);
 
+module.exports = router;
 
+//RUTA Editorial
+var EditorialController = require("../controllers/Editorial-controller"),
+  express = require("express"),
+  router = express.Router();
 
+router
   //*** EDITORIAL ***
   .get("/editorial/getall", EditorialController.getAll)
   .get("/editorial/getone/:numeroeditorial", EditorialController.getOne)
   .post("/editorial/insertar/:numeroeditorial", EditorialController.post)
   .put("/editorial/actualizar/:numeroeditorial", EditorialController.put)
   .delete("/editorial/eliminar/:numeroeditorial", EditorialController.delete)
-  .use(LibrosController.error404)
-  .use(EditorialController.error404)
-  .use(EscritorController.error404);
-  
+  .use(EditorialController.error404);
+
 module.exports = router;
 
 
